e8af3ae3319e186fb667eaa7135f63656713b2fd
M.Daneshjoo <47611839+mdaneshjoo@users.noreply.github.com>
2022-05-09 12:06:56 +0300

    add file extentions

---
+++ rand_num_generator.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION generate_random_number() 
+RETURNS int AS $generate_random_number$ 
+	DECLARE
+		randNumber int :=FLOOR(random()* (high-low + 1) + low); 
+		i int := 0;
+	BEGIN
+		WHILE (SELECT rand FROM the_table WHERE rand = randNumber) IS NOT null AND i < 10 
+		LOOP
+			randNumber :=FLOOR(random()* (high-low + 1) + low); 
+			RAISE NOTICE 'randNumber %',randNumber;
+			RAISE NOTICE 'result in table %',(SELECT rand FROM the_table WHERE rand = randNumber);
+			i := i+1;
+		END LOOP;
+		RETURN randNumber;
+	END;
+$generate_random_number$ language 'plpgsql' STRICT;
